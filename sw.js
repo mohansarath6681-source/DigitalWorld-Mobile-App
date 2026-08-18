a67082073981637b1e2fdd702a2933dfaf3fec7c
mohansarath6681-source <mohansarath6681@gmail.com>
2026-08-18 22:40:30 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='digital-world-v1';
+const FILES=['./','./index.html','./manifest.json','./logo.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
