ea3f6209a9373a7104e1cd06a774aa191a1643b7
mohansarath6681-source <mohansarath6681@gmail.com>
2026-08-20 09:21:24 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='digital-world-v1';
-const FILES=['./','./index.html','./manifest.json','./logo.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
